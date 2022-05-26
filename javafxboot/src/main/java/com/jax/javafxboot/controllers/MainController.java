8f80b935027e5117333624d07652429d8f0d2b40
jaxtony <hatrongvu13@gmail.com>
2022-05-27 01:14:05 +0700

    update code stage init fxml

--- javafxboot/src/main/java/com/jax/javafxboot/controllers/MainController.java
+++ javafxboot/src/main/java/com/jax/javafxboot/controllers/MainController.java
@@ -1,2 +1,15 @@
-package com.jax.javafxboot.controllers;public class MainController {
+package com.jax.javafxboot.controllers;
+
+import javafx.fxml.Initializable;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+public class MainController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
